700c0f6eea80aa94b608b4f4fb6c3b90d1618637
FrederickMih <fredtimermih@gmail.com>
2021-02-17 21:16:12 +0100

    added the scraper class

--- lib/web-scraper.rb
+++ lib/web-scraper.rb
@@ -1,2 +1,12 @@
 require 'nokogiri'
+require 'httparty'
+require 'byebug'
 require 'open-uri'
+
+class Scraper
+   def initialize(url)
+      request = HTTParty.get(url)
+      @response = Nokogiri::HTML(request)
+   end
+   
+end
