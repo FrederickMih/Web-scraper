8c346d78b95cf33cc66bc69aa012bd0bb2db24a7
FrederickMih <fredtimermih@gmail.com>
2021-02-18 08:34:01 +0100

    added the table_regist method

--- lib/web-scraper.rb
+++ lib/web-scraper.rb
@@ -10,7 +10,21 @@ class Scraper
    end
 
    def countries(response)
-      response.css('table#main_table_countries_today > tbody > tr').map(&:text).count  
+      response.css('table#main_table_countries_today').map(&:text).count  
+   end
+
+   # method that select and a  return covid table from the site with its index
+   def  table_regist(response, index)
+      scrape = response.css('table#main_table_countries_today[#{index}]').text
+      regist = scrape.split("").delete '%'
+      {
+         "index": regist[0]
+         "countries": regist[1]
+         "total_cases": regist[2]
+         "tatal_deaths": regist[3]
+         "total_recovered": regist[4]
+         "total_tests": regist[5]
+      } 
    end
    
 end
